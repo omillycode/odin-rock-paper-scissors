df8f0a6830cdcceeb3922a407f33ccc7bf10b8c0
omillycode <melbournemacdonald97@gmail.com>
2021-12-11 03:11:30 -0400

    Added computerPlays function, randomly selects between Rock, Paper, or Scissors.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-/*  */
+/* choose from either rock, paper, or scissors randomly */
 function computerPlay() {
-
+   let choices = [
+       "Rock",
+       "Paper",
+       "Scissors"
+   ]
+   let randomSelection = choices[Math.floor(Math.random()*choices.length)]
+   return randomSelection;
 }
