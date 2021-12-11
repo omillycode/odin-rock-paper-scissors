997ec2b963a128953f7926006d980c35d59c2e8b
omillycode <melbournemacdonald97@gmail.com>
2021-12-11 03:22:26 -0400

    playRound function added with prompt to select a player's choice.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 /* choose from either rock, paper, or scissors randomly */
 function computerPlay() {
    let choices = [
-       "Rock",
-       "Paper",
-       "Scissors"
+       "rock",
+       "paper",
+       "scissors"
    ]
    let randomSelection = choices[Math.floor(Math.random()*choices.length)]
    return randomSelection;
+}
+/* plays a single round of rock, paper, scissors with player selection */
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Which will you choose?", "Rock, Paper, or Scissors?")
 }
