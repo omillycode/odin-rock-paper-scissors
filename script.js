58ff3440c7940a812786dbcc3d15a4ab5b413bad
omillycode <melbournemacdonald97@gmail.com>
2021-12-11 03:51:53 -0400

    if statement for each of the winning possibilities.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+let rock = "rock";
+let paper = "paper";
+let scissors = "scissors";
 /* choose from either rock, paper, or scissors randomly */
+
 function computerPlay() {
-   let choices = [
-       "rock",
-       "paper",
-       "scissors"
+    let choices = [
+       rock,
+       paper,
+       scissors
    ]
    let randomSelection = choices[Math.floor(Math.random()*choices.length)]
    return randomSelection;
 }
-/* plays a single round of rock, paper, scissors with player selection */
+/* asks for players choice, compares it to random choice, and decides who wins the round */
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Which will you choose?", "Rock, Paper, or Scissors?")
+    playerSelection = prompt("Which will you choose?", "Rock, Paper, or Scissors?").toLowerCase();
+    const computerSelection = computerPlay;
+    rock > scissors;
+    
+    if (playerSelection === computerSelection) {
+        return("It's a tie");
+    } else if (playerSelection < computerSelection) {
+        return("Try Again");
+    }else {
+        return("You've won!");
+    }
+    
 }
