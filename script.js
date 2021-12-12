a10d09405ce8fdc4bbf674c474e8e90c2b79e86b
omillycode <melbournemacdonald97@gmail.com>
2021-12-12 15:03:17 -0400

    Finished the playRound function which selects random choice, and player choice to determine a winner.

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissors";
 /* choose from either rock, paper, or scissors randomly */
-
 function computerPlay() {
     let choices = [
-       rock,
-       paper,
-       scissors
+       "rock",
+       "paper",
+       "scissors"
    ]
    let randomSelection = choices[Math.floor(Math.random()*choices.length)]
    return randomSelection;
 }
+let playerChoice = prompt("Which will you choose?", "rock, paper, or scissors?").toLowerCase();
+let playerSelection = playerChoice
+let computerSelection = computerPlay();
 /* asks for players choice, compares it to random choice, and decides who wins the round */
-
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Which will you choose?", "Rock, Paper, or Scissors?").toLowerCase();
-    computerSelection = computerPlay;
-
     if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            return "You Win!";
-        }else if (computerSelection === "paper") {
-            return "Try Again!";
-        }else if (computerSelection === playerSelection) {
-            return "It's A Tie!";
+            return ("You Win!");
+        }
+        if (computerSelection === "paper") {
+            return ("Try Again!");
+        }
+        if (computerSelection === playerSelection) {
+            return ("It's A Tie!");
         }
     }
     if (playerSelection === "scissors") {
