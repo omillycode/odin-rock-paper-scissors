efc146413d91ad942d6aeab40ded671fa0e76df0
omillycode <melbournemacdonald97@gmail.com>
2022-03-02 23:07:07 -0400

    Create gameTracker element that keeps both the player score and PC score to show to player, and an end of game result after 5 rounds

--- script.js
+++ script.js
@@ -24,9 +24,11 @@ let computerPoints = 0
 
 const scoreDiv = document.createElement('div')
 const theResult = document.createElement('p')
+const gameTracker = document.createElement('p')
 const mainPage = document.getElementById('mainPage')
 mainPage.appendChild(scoreDiv)
 scoreDiv.appendChild(theResult)
+scoreDiv.appendChild(gameTracker)
 
 
 /* gets players choice, compares it to random choice, and decides who wins the round */
@@ -34,51 +36,56 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
     if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            theResult.textContent = `You Win! You:${playerPoints}, PC:${computerPoints}`;
+            theResult.textContent = `You Win! `;
             return ++playerPoints;
         }
         if (computerSelection === "paper") {
-            theResult.textContent = `You Lose! You:${playerPoints}, PC:${computerPoints}`;
+            theResult.textContent = `You Lose! `;
             return ++computerPoints;
         }
         if (computerSelection === playerSelection) {
             theResult.textContent = "It's a tie";
         }
+        gameTracker.textContent = `You:${playerPoints}, PC:${computerPoints}`;
     }
     if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
-            theResult.textContent = `You Win! You:${playerPoints}, PC:${computerPoints}`;
+            theResult.textContent = `You Win! `;
             return ++playerPoints;
         }
         if (computerSelection === "rock") {
-            theResult.textContent = `You Lose! You:${playerPoints}, PC:${computerPoints}`;
+            theResult.textContent = `You Lose! `;
             return ++computerPoints;
         }
         if (computerSelection === playerSelection) {
             theResult.textContent = "It's a tie";
         }
+        gameTracker.textContent = `You:${playerPoints}, PC:${computerPoints}`;
     }
     if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            theResult.textContent = `You Win! You:${playerPoints}, PC:${computerPoints}`;
+            theResult.textContent = `You Win! `;
             return ++playerPoints;
         }
         if (computerSelection === "scissors") {
-            theResult.textContent = `You Lose! You:${playerPoints}, PC:${computerPoints}`;
+            theResult.textContent = `You Lose! `;
             return ++computerPoints;
         }
         if (computerSelection === playerSelection) {
             theResult.textContent = "It's a tie";
         }
+        gameTracker.textContent = `You:${playerPoints}, PC:${computerPoints}`;
     }
-
+    
 }
-if (playerPoints >= 5 && playerPoints > computerPoints) {
-    theResult.textContent = "Congrats!";
-} 
-if (computerPoints >= 5 && computerPoints > playerPoints) {
-    theResult.textContent = "Try Again!";
+if (playerPoints > computerPoints && playerPoints >= 5) {
+    playerPoints = 0;
+    computerPoints = 0;
+    theResult.textContent = "Congrats, you've won!";
+} else if (computerPoints > playerPoints && computerPoints >= 5) {
+    playerPoints = 0;
+    computerPoints = 0;
+    theResult.textContent = "Sorry, please try again!";
 }
-
 
 
